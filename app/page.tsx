cde4240c9ce2992cdfacc1b49f1d539f8bfe2481
Peyal Hasan <peyalhasan143@gmail.com>
2026-04-11 20:20:34 +0600

    Update metadata,font,color

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import React from "react";
 const HomePage = () => {
   const { data, error, isLoading } = useGetUsersQuery();
   // console.log("data", data, error, isLoading);
-  return <div className="">Home page</div>;
+  return <div className="text-primary">Home page
+  
+  <h1 className=" text-secondary">ফ্রি রেজিস্ট্রেশন করে আজই শুরু করুন আপনার ড্রপশিপিং বিজনেস। কোনো ইনভেস্টমেন্ট ছাড়াই ইনকাম করার সুযোগ!</h1>
+  </div>;
 };
 
 export default HomePage;
