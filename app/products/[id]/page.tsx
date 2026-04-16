9dea737e817c3b906c852b3667c0aea3529c23d7
kamrulmiazi <kamrul116522@gmail.com>
2026-04-16 19:56:40 +0600

    Added seo in product details view page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,9 +2,35 @@ import React from 'react';
 import productsData from "@/public/assets/products.json";
 import { Product } from '@/app/types/productTypes';
 import ProductDetails from '@/app/components/products/productDetails';
+import { Metadata } from 'next';
+
+// Product SEO
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const products: Product[] = productsData || [];
+  const product = products.find((p) => p.id === params.id);
+
+  if (!product) {
+    return {
+      title: "Product Not Found",
+    };
+  }
+
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [product.thumbnail],
+    },
+  };
+}
 
 const page = async({params}:{params: { id: string }}) => {
-
     const {id} = await params;
   const products: Product[] = productsData || [];
 
