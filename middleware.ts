809613102de3ffec4d6fe40c28a791f6df9a3188
kamrulmiazi <kamrul116522@gmail.com>
2026-04-30 20:06:03 +0600

    off middelwear

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(req: NextRequest) {
-  const token = await req.cookies.get("accessToken")?.value;
-  const allToken = await req.cookies.getAll();
-  console.log(token);
-  console.log({ allToken });
+// // This function can be marked `async` if using `await` inside
+// export async function middleware(req: NextRequest) {
+//   const token = await req.cookies.get("accessToken")?.value;
+//   const allToken = await req.cookies.getAll();
+//   console.log(token);
+//   console.log({ allToken });
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  } else {
-    return NextResponse.next();
-  }
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   } else {
+//     return NextResponse.next();
+//   }
 
-  return NextResponse.redirect(new URL("/", req.url));
-  // next();
-}
+//   return NextResponse.redirect(new URL("/", req.url));
+//   // next();
+// }
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ["/about/:path*", "/dashboard/:path*", "/products/:path*"],
-};
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: ["/about/:path*", "/dashboard/:path*", "/products/:path*"],
+// };
